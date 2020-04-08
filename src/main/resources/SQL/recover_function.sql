0a4ef75d3b767b77bfdf8d5bbbdf02c7ccf4d266
Korolenko Roman <korolenko.as1204@asugubkin.ru>
2020-04-08 12:31:17 +0300

    Added recover function, fix merge with hist

--- src/main/resources/SQL/recover_function.sql
+++ src/main/resources/SQL/recover_function.sql
@@ -7,20 +7,20 @@ begin
 
 	raise notice 'TARGET table has been cleaned since %', recover_date;
 
-/*отбираем самые свежие данный из HIST за указанный срез данных*/
-insert into bank_additional_merge
-select * from bank_additional_hist bah
-where valid_from >= recover_date
-and valid_to is null;
-raise notice 'TARGET table has been updated from HIST since %', recover_date;
+  /*отбираем самые свежие данный из HIST за указанный срез данных*/
+  insert into bank_additional_merge
+    select * from bank_additional_hist bah
+    where valid_from >= recover_date
+      and valid_to is null;
+  raise notice 'TARGET table has been updated from HIST since %', recover_date;
 
-return 0;
+  return 0;
 
-exception
-when others then
-RAISE INFO 'Error Name:%',SQLERRM;
-RAISE INFO 'Error State:%', SQLSTATE;
-return -1;
+  exception
+  when others then
+    RAISE INFO 'Error Name:%',SQLERRM;
+    RAISE INFO 'Error State:%', SQLSTATE;
+    return -1;
 end;
 $$
 LANGUAGE plpgsql;
