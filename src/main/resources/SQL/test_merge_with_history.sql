0b0af8b1237149de3b2af379ff9d5dfe6fe32cf6
Korolenko Roman <korolenko.as1204@asugubkin.ru>
2020-04-08 12:34:37 +0300

    Added recover function, fix merge with hist

--- src/main/resources/SQL/test_merge_with_history.sql
+++ src/main/resources/SQL/test_merge_with_history.sql
@@ -4,11 +4,11 @@ begin
 	/*блокируем инкрементальную таблицу на запрет изменения данных в ней
 	 до момента окончания работы процедуры*/
 	LOCK TABLE public.bank_additional_inc IN ROW EXCLUSIVE MODE;
-	
+
 	LOCK TABLE public.bank_additional_merge IN ROW EXCLUSIVE MODE;
 	/*если обнаружена связка по ключу в TARGET-таблице с инкрементальными данными, делаем insert "устаревших"
 	 * данных из TARGET в HIST*/
-	insert into bank_additional_hist 
+	insert into bank_additional_hist
 			select 	bam."age",
 					bam.job,
 					bam.marital,
@@ -32,11 +32,11 @@ begin
 					bam.y,
 					bam.valid_from,
 					actual_inc.valid_from, -- проставляем признак "устаревшей" записи
-					bam.merge_operation  
+					bam.merge_operation
 	from bank_additional_merge bam
-	inner join 
+	inner join
 		(		/*отбираем самые свежие записи по каждому ключу, пришедшие в INC*/
-						select * from 
+						select * from
 						(
 						select row_number() over(partition by duration order by valid_from desc) as rn,
 								duration,
@@ -47,7 +47,7 @@ begin
 					)actual_inc
 	on bam.duration = actual_inc.duration
 	and bam.valid_from < actual_inc.valid_from;
-	
+
 	/*меняем в TARGET устаревшие записи на новые*/
 	update bank_additional_merge target
 		set	"age" = to_update."age",
@@ -78,7 +78,7 @@ begin
 	inner join
 	(
 	/*отбираем самые свежие записи по каждому ключу, пришедшие в INC*/
-		select * from 
+		select * from
 		(
 			select row_number() over(partition by duration order by valid_from desc) as rn,
 					duration,
@@ -91,10 +91,10 @@ begin
 	and to_update.valid_from = actual_inc.valid_from
 	where target.duration = to_update.duration
 	and target.valid_from < to_update.valid_from;
-	
-	
+
+
 	/*если в INC имеется больше 1 записи по ключу, всем кроме самой свежей корректно проставляем признак устаревшей записи и записываем их в HIST*/
-	insert into bank_additional_hist 
+	insert into bank_additional_hist
 	select ins_val."age",
 					ins_val.job,
 					ins_val.marital,
@@ -120,17 +120,17 @@ begin
 					ins_val.new_valid_to,
 					ins_val.merge_operation
 	from (
-		select 
+		select
 			lead(a.valid_from, 1) over (partition by a.duration order by a.valid_from) as new_valid_to,
-			* 
+			*
 			from (
 			select * from bank_additional_inc bai
 			union
-			select  * from bank_additional_merge 
+			select  * from bank_additional_merge
 			where duration in (select distinct duration from bank_additional_inc) /*если в INC пришла устаревшая запись*/
-			and valid_from >= (select min(valid_from) from bank_additional_inc) 
+			and valid_from >= (select min(valid_from) from bank_additional_inc)
 			union
-			select  * from bank_additional_hist 
+			select  * from bank_additional_hist
 			where duration in (select distinct duration from bank_additional_inc) /*2 последних условия, чтобы не тянуть все что уже есть в истории*/
 			and valid_from >= (select min(valid_from) from bank_additional_inc)  /*но при этом корректно обработать ситуацию, когда в INC есть данные по одному*/
 			)a																	/*ключу одновременно и старше и свежее того, что есть в TARGET*/
@@ -142,13 +142,16 @@ begin
 	where bah.duration = ins_val.duration
 		and bah.valid_from = ins_val.valid_from);
 		--and bah.valid_to = new_valid_to);
-	
+
 	/*записываем в TARGET оставшиеся данные из INC, по которым нет связки по ключу*/
-	insert into bank_additional_merge 
+	insert into bank_additional_merge
 	select * from bank_additional_inc bac
 	where not exists (
 	select 1 from bank_additional_merge bam
 	where bac.duration = bam.duration);
+
+	/*очищаем INC таблицу*/
+	truncate table public.bank_additional_inc;
 
 	return 0;
 
